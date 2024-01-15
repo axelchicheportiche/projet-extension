e48581c2d531ba69fea5ebf2c57537bc7f5746e7
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-01-15 12:23:45 +0100

    ajout erase popup

--- popup.js
+++ popup.js
@@ -15,10 +15,10 @@ async function getWebsiteList() {
       // Récupérer la liste des sites web
       let websiteList = await getWebsiteList();
         console.log(websiteList)
-      // Votre code existant pour afficher la liste ou initialiser une liste vide
+      //  afficher la liste
       if (Array.isArray(websiteList)) {
-        for (let i = 0; i < websiteList.length; i++) {
-          document.getElementById("arrayWebsite").innerHTML += `<div class="site">${websiteList[i]}</div>`;
+        for (let i = websiteList.length - 1; i >= 0; i--) {
+          document.getElementById("arrayWebsite").innerHTML += `<div class="site">${websiteList[i]}<button id="erase${[i]}"> - </button></div>`;
         }
       } else {
         websiteList = [];
@@ -28,11 +28,12 @@ async function getWebsiteList() {
       document.getElementById("buttonAdd").addEventListener("click", function () {
         let valeurInput = document.getElementById("inputAdd").value;
         if (valeurInput.length > 0 && valeurInput.startsWith("https://www.")) {
-          websiteList.unshift(valeurInput);
+          websiteList.push(valeurInput);
 
           let oldHTML = document.getElementById("arrayWebsite").innerHTML;
           document.getElementById("arrayWebsite").innerHTML =
-            `<div class="site">${websiteList[0]}</div>`+ oldHTML;
+            `<div class="site">${websiteList[websiteList.length-1]}<button id="erase${websiteList.length-1}">-</div>`+ oldHTML;
+            console.log(websiteList)
           document.getElementById("inputAdd").value = "";
 
           // Mettre à jour la liste dans le stockage local
@@ -49,10 +50,31 @@ async function getWebsiteList() {
         console.log(websiteList);
       });
 
+
+      //EFFACER ELEMENT DE LA LISTE
+      for (let i = 0; i < websiteList.length; i++) {
+        let eraseButton = document.getElementById(`erase${i}`);
+        eraseButton.addEventListener("click", function () {
+          websiteList.splice(i,1)
+          // Mettre à jour la liste dans le stockage local
+          chrome.storage.local.set({ websiteList: websiteList }, function() {
+            // Envoyer un message pour indiquer que la liste a été mise à jour
+            chrome.runtime.sendMessage({ action: "popupListUpdate", nouvelleListe: websiteList });
+          });
+          console.log("check tab", websiteList)
+          document.getElementById("arrayWebsite").innerHTML ="";
+          for (let i = websiteList.length - 1; i >= 0; i--) {
+            document.getElementById("arrayWebsite").innerHTML += `<div class="site">${websiteList[i]}<button id="erase${[i]}"> - </button></div>`;
+          }
+            console.log(`Bouton ${eraseButton.id} cliqué.`);
+            location.reload()
+        });
+    }
     } catch (error) {
       console.error("Erreur lors de la récupération de la liste des sites web:", error);
     }
   }
+
 
   // Appel de la fonction principale asynchrone
   main();
